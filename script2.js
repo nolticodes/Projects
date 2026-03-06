402801302c2c29e7d4d46fd7b003b08658cf3a3c
Denis Nolting <denis.nolting@gmx.de>
2026-03-06 09:14:09 +0100

    review marco

--- script2.js
+++ script2.js
@@ -1,5 +1,4 @@
-
-//IMAGE ARRAY
+// PARAMETER
 const imgArray = [
     "./img/gallery/anime.jpg",
     "./img/gallery/bird_white.jpg",
@@ -13,77 +12,73 @@ const imgArray = [
     "./img/gallery/mountain_sky.jpg",
     "./img/gallery/winter_mountains.jpg",
     "./img/gallery/wintertree.jpg",
-]
+]; 
 
-// BILDER IN ÜBERSICHT LADEN
-const imgOverviewRef = document.getElementById("image_overview_id")
+const imgOverviewRef = document.getElementById("image_overview_id"); 
 
+const dialogRef = document.getElementById("image_view_dialog");
+
+let currentIndex = "";
+
+const imageInDialogRef = document.getElementById("image_open_in_dialog"); 
+
+const titleImageInDialogRef = document.getElementById("dialog_image_name");
+
+const imageCounterRef = document.getElementById("image_counter"); 
+
+const prevButtonRef = document.getElementById("btn_prev");
+
+// RENDER IMG IN OVERVIEW
 function renderImages() {
     for(i=0; i<imgArray.length; i++) {
-        imgOverviewRef.innerHTML += `<img onclick="openImageDialog(${[i]})" id="image${[i]}" src="${imgArray[i]}"></img>`;
+        imgOverviewRef.innerHTML += `<img onclick="openImageDialog(${[i]})" 
+                                    id="image${[i]}" 
+                                    alt="${imgArray[i]}"
+                                    src="${imgArray[i]}"></img>`
+                                    
     }
 }
 
-// DIALOG ÖFFNEN & SCHLIESSEN
-const dialogRef = document.getElementById("image_view_dialog");
-let currentIndex = "";
-
-function setCurrentIndex(i){
-    currentIndex = i
-}
-
+// OPEN & CLOSE DIALOG
 function openImageDialog(i) {
     dialogRef.showModal();
     renderImageInDialog(i);
     loadTitleToDialog(i);
     imageCounterFctn(i);
-    setCurrentIndex(i);
+    currentIndex = i
 }
 
 function closeImageDialog() {
     dialogRef.close();
 }
 
-// BILD IN DIALOG LADEN
-const imageInDialogRef = document.getElementById("image_open_in_dialog"); 
-
+// RENDER IMG IN DIALOG
 function renderImageInDialog(i) {
     imageInDialogRef.innerHTML = `<img src="${imgArray[i]}"> </img>`;
 }
 
-// TITEL IN DIALOG LADEN
-const titleImageInDialogRef = document.getElementById("dialog_image_name");
-
+// RENDER TITLE IN DIALOG
 function loadTitleToDialog(i) {
     titleImageInDialogRef.innerHTML = imgArray[i].replace("_", " ").replace("_", " ").slice(14, -4);
 }
 
-// COUNTER IN DIALOG LADEN 
-const imageCounterRef = document.getElementById("image_counter"); 
-
+// RENDER COUTNER IN DIALOG
 function imageCounterFctn(i){ 
     imageCounterRef.innerHTML = i+1 + " / " + imgArray.length;
 }
 
-// BUTTON ZURÜCK
-const prevButtonRef = document.getElementById("btn_prev")
-
+// BUTTON BACK
 function prevImage(){
     currentIndex = (currentIndex - 1 + imgArray.length) % imgArray.length;
-    imageCounterFctn(currentIndex);
     imageCounterFctn(currentIndex);
     renderImageInDialog(currentIndex);
     loadTitleToDialog(currentIndex);
 }
 
-// BUTTON WEITER
+// BUTTON NEXT
 function nextImage(){
     currentIndex = (currentIndex + 1) % imgArray.length;
     imageCounterFctn(currentIndex);
     renderImageInDialog(currentIndex);
     loadTitleToDialog(currentIndex);
 }
-
-
-
-
